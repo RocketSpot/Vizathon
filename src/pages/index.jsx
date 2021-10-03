ae4faa04a466a42f4245cded2532882b01d8c70c
Adam Ahmed <75999418+RocketSpot@users.noreply.github.com>
2021-10-03 14:13:44 -0400

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -19,6 +19,8 @@ export default function Home() {
 			<Hero />
 			<Container py={20} bgColor="black" align="center" justify="center">
 				<VStack maxW="1280px">
+			<Container py={20} bgColor="black" align="center" justify="center">
+				<VStack maxW="1280px">
 					<Flex justify="center" flexWrap="wrap">
 						<Box align="center" justify="center">
 							<Mapbox />
